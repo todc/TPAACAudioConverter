ee7424970285fe532183d036c23632dcd81856fd
Tim O'Donnell <tim.odonnell@imperva.com>
2014-03-13 17:42:32 -0700

    Update sample app for ARC, objective-c

--- AACConverter/AACConverterViewController.m
+++ AACConverter/AACConverterViewController.m
@@ -13,11 +13,18 @@
 #define checkResult(result,operation) (_checkResult((result),(operation),__FILE__,__LINE__))
 static inline BOOL _checkResult(OSStatus result, const char *operation, const char* file, int line) {
     if ( result != noErr ) {
-        NSLog(@"%s:%d: %s result %d %08X %4.4s\n", file, line, operation, (int)result, (int)result, (char*)&result); 
+        NSLog(@"%s:%d: %s result %d %08X %4.4s\n", file, line, operation, (int)result, (int)result, (char*)&result);
         return NO;
     }
     return YES;
 }
+
+
+@interface AACConverterViewController ()
+{
+    BOOL isAudioSessionInitialized;
+}
+@end
 
 
 @implementation AACConverterViewController
@@ -28,43 +35,39 @@ static inline BOOL _checkResult(OSStatus result, const char *operation, const ch
 @synthesize spinner;
 
 // Callback to be notified of audio session interruptions (which have an impact on the conversion process)
-static void interruptionListener(void *inClientData, UInt32 inInterruption)
+- (void)audioSessionDidChangeInterruptionType:(NSNotification *)notification
 {
-	AACConverterViewController *THIS = (AACConverterViewController *)inClientData;
-	
-	if (inInterruption == kAudioSessionEndInterruption) {
-		// make sure we are again the active session
+    AVAudioSessionInterruptionType interruptionType = [[[notification userInfo] objectForKey:AVAudioSessionInterruptionTypeKey] unsignedIntegerValue];
+    if (AVAudioSessionInterruptionTypeBegan == interruptionType)
+    {
+        // make sure we are again the active session
 		checkResult(AudioSessionSetActive(true), "resume audio session");
-        if ( THIS->audioConverter ) [THIS->audioConverter resume];
-	}
-	
-	if (inInterruption == kAudioSessionBeginInterruption) {
-        if ( THIS->audioConverter ) [THIS->audioConverter interrupt];
+        if ( self->audioConverter ) [self->audioConverter resume];
+    }
+    else if (AVAudioSessionInterruptionTypeEnded == interruptionType)
+    {
+        if ( self->audioConverter ) [self->audioConverter interrupt];
     }
 }
 
-- (void)dealloc
-{
-    [convertButton release];
-    [playConvertedButton release];
-    [emailConvertedButton release];
-    [progressView release];
-    [spinner release];
-    [progressView release];
-    [spinner release];
-    [super dealloc];
-}
-
 #pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    isAudioSessionInitialized = NO;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(audioSessionDidChangeInterruptionType:)
+                                                 name:AVAudioSessionInterruptionNotification
+                                               object:[AVAudioSession sharedInstance]];
+}
 
 - (void)viewDidUnload
 {
     [self setConvertButton:nil];
     [self setPlayConvertedButton:nil];
     [self setEmailConvertedButton:nil];
-    [progressView release];
     progressView = nil;
-    [spinner release];
     spinner = nil;
     [self setProgressView:nil];
     [self setSpinner:nil];
@@ -76,7 +79,6 @@ static void interruptionListener(void *inClientData, UInt32 inInterruption)
 - (IBAction)playOriginal:(id)sender {
     if ( audioPlayer ) {
         [audioPlayer stop];
-        [audioPlayer release];
         audioPlayer = nil;
         [(UIButton*)sender setTitle:@"Play original" forState:UIControlStateNormal];
     } else {
@@ -89,40 +91,40 @@ static void interruptionListener(void *inClientData, UInt32 inInterruption)
 
 - (IBAction)convert:(id)sender {
     if ( ![TPAACAudioConverter AACConverterAvailable] ) {
-        [[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Converting audio", @"")
-                                     message:NSLocalizedString(@"Couldn't convert audio: Not supported on this device", @"")
-                                    delegate:nil
-                           cancelButtonTitle:nil
-                           otherButtonTitles:NSLocalizedString(@"OK", @""), nil] autorelease] show];
+        [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Converting audio", @"")
+                                    message:NSLocalizedString(@"Couldn't convert audio: Not supported on this device", @"")
+                                   delegate:nil
+                          cancelButtonTitle:nil
+                          otherButtonTitles:NSLocalizedString(@"OK", @""), nil] show];
         return;
     }
     
-    // Initialise audio session, and register an interruption listener, important for AAC conversion
-    if ( !checkResult(AudioSessionInitialize(NULL, NULL, interruptionListener, self), "initialise audio session") ) {
-        [[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Converting audio", @"")
-                                     message:NSLocalizedString(@"Couldn't initialise audio session!", @"")
-                                    delegate:nil
-                           cancelButtonTitle:nil
-                           otherButtonTitles:NSLocalizedString(@"OK", @""), nil] autorelease] show];
+    NSError *initError;
+    BOOL activated = [[AVAudioSession sharedInstance] setActive:YES error:&initError];
+    if (initError != nil || !activated) {
+        [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Converting audio", @"")
+                                    message:NSLocalizedString(@"Couldn't initialise audio session!", @"")
+                                   delegate:nil
+                          cancelButtonTitle:nil
+                          otherButtonTitles:NSLocalizedString(@"OK", @""), nil] show];
         return;
     }
     
-    
-    // Set up an audio session compatible with AAC conversion.  Note that AAC conversion is incompatible with any session that provides mixing with other device audio.
-    UInt32 audioCategory = kAudioSessionCategory_MediaPlayback;
-    if ( !checkResult(AudioSessionSetProperty(kAudioSessionProperty_AudioCategory, sizeof(audioCategory), &audioCategory), "setup session category") ) {
-        [[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Converting audio", @"")
-                                     message:NSLocalizedString(@"Couldn't setup audio category!", @"")
-                                    delegate:nil
-                           cancelButtonTitle:nil
-                           otherButtonTitles:NSLocalizedString(@"OK", @""), nil] autorelease] show];
+    NSError *nsError;
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&nsError];
+    if (nsError != nil) {
+        [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Converting audio", @"")
+                                    message:NSLocalizedString(@"Couldn't setup audio category!", @"")
+                                   delegate:nil
+                          cancelButtonTitle:nil
+                          otherButtonTitles:NSLocalizedString(@"OK", @""), nil] show];
         return;
-    } 
+    }
     
     NSArray *documentsFolders = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    audioConverter = [[[TPAACAudioConverter alloc] initWithDelegate:self 
-                                                             source:[[NSBundle mainBundle] pathForResource:@"audio" ofType:@"aiff"]
-                                                        destination:[[documentsFolders objectAtIndex:0] stringByAppendingPathComponent:@"audio.m4a"]] autorelease];
+    audioConverter = [[TPAACAudioConverter alloc] initWithDelegate:self
+                                                            source:[[NSBundle mainBundle] pathForResource:@"audio" ofType:@"aiff"]
+                                                       destination:[[documentsFolders objectAtIndex:0] stringByAppendingPathComponent:@"audio.m4a"]];
     ((UIButton*)sender).enabled = NO;
     [self.spinner startAnimating];
     self.progressView.progress = 0.0;
@@ -134,7 +136,6 @@ static void interruptionListener(void *inClientData, UInt32 inInterruption)
 - (IBAction)playConverted:(id)sender {
     if ( audioPlayer ) {
         [audioPlayer stop];
-        [audioPlayer release];
         audioPlayer = nil;
         [(UIButton*)sender setTitle:@"Play converted" forState:UIControlStateNormal];
     } else {
@@ -154,7 +155,7 @@ static void interruptionListener(void *inClientData, UInt32 inInterruption)
     MFMailComposeViewController *mailController = [[MFMailComposeViewController alloc] init];
     mailController.mailComposeDelegate = self;
     [mailController setSubject:NSLocalizedString(@"Recording", @"")];
-    [mailController addAttachmentData:[NSData dataWithContentsOfMappedFile:path] 
+    [mailController addAttachmentData:[NSData dataWithContentsOfMappedFile:path]
                              mimeType:@"audio/mp4a-latm"
                              fileName:[path lastPathComponent]];
     
@@ -177,11 +178,11 @@ static void interruptionListener(void *inClientData, UInt32 inInterruption)
 }
 
 -(void)AACAudioConverter:(TPAACAudioConverter *)converter didFailWithError:(NSError *)error {
-    [[[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Converting audio", @"")
-                                 message:[NSString stringWithFormat:NSLocalizedString(@"Couldn't convert audio: %@", @""), [error localizedDescription]]
-                                delegate:nil
-                       cancelButtonTitle:nil
-                       otherButtonTitles:NSLocalizedString(@"OK", @""), nil] autorelease] show];
+    [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Converting audio", @"")
+                                message:[NSString stringWithFormat:NSLocalizedString(@"Couldn't convert audio: %@", @""), [error localizedDescription]]
+                               delegate:nil
+                      cancelButtonTitle:nil
+                      otherButtonTitles:NSLocalizedString(@"OK", @""), nil] show];
     self.convertButton.enabled = YES;
     audioConverter = nil;
 }
